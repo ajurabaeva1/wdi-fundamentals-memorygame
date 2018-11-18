048cb1d64a3545c7bc44720d58fa53570c9e2eec
AZIZA JURABAEVA <44528324+ajurabaeva1@users.noreply.github.com>
2018-11-17 22:55:40 -0500

    Delete main.js

--- wdi-fundamentals-memorygame/memory_game/main.js
+++
@@ -1,9 +0,0 @@
-console.log("Up and running!");
-
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardThree = "king";
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
